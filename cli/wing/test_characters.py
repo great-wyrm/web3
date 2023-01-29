8647eceb310ee90ae4da68bd1559ae1c455aeb2e
Neeraj Kashyap <nkashy1@gmail.com>
2023-01-29 09:33:44 -0800

    Test minting logic

--- cli/wing/test_characters.py
+++ cli/wing/test_characters.py
@@ -10,6 +10,7 @@ from .core import characters_gogogo
 
 MAX_UINT = 2**256 - 1
 
+
 class CharactersTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
@@ -48,9 +49,6 @@ class CharactersTestCase(unittest.TestCase):
             cls.admin.address, cls.admin_terminus_pool_id, 1, "", cls.owner_tx_config
         )
 
-        # Mint character creation badges to player account
-        cls.terminus.mint(cls.player.address, cls.character_creation_terminus_pool_id, 10, "", cls.owner_tx_config)
-
         cls.contract_name = "Great Wyrm Test Characters"
         cls.contract_symbol = "WYRMTEST"
         cls.contract_uri = "https://example.com"
@@ -70,11 +68,19 @@ class CharactersTestCase(unittest.TestCase):
             cls.deployed_contracts["contracts"]["Diamond"]
         )
 
+        # Approve Characters contract for the character creation Terminus pool.
+        cls.terminus.approve_for_pool(cls.character_creation_terminus_pool_id, cls.characters.address, cls.owner_tx_config)
+
 
 class CharactersSetupTests(CharactersTestCase):
     def test_characters_setup(self):
-        current_block = len(chain)
-        contract_information_set_events = _fetch_events_chunk(web3_client, characters_events.CONTRACT_INFORMATION_SET, 0, current_block, self.characters.address)
+        contract_information_set_events = _fetch_events_chunk(
+            web3_client,
+            characters_events.CONTRACT_INFORMATION_SET,
+            self.predeployment_block,
+            self.postdeployment_block,
+            self.characters.address,
+        )
         self.assertEqual(len(contract_information_set_events), 1)
         event = contract_information_set_events[0]
         self.assertEqual(event["event"], "ContractInformationSet")
@@ -85,3 +91,98 @@ class CharactersSetupTests(CharactersTestCase):
         self.assertEqual(self.characters.name(), self.contract_name)
         self.assertEqual(self.characters.symbol(), self.contract_symbol)
         self.assertEqual(self.characters.contract_uri(), self.contract_uri)
+
+
+class CharacterCreationTests(CharactersTestCase):
+    def test_character_creation_requires_character_creation_terminus_token(self):
+        self.assertEqual(
+            self.terminus.balance_of(
+                self.random_person, self.character_creation_terminus_pool_id
+            ),
+            0,
+        )
+
+        with self.assertRaises(VirtualMachineError):
+            self.characters.mint(
+                self.random_person.address, {"from": self.random_person}
+            )
+
+        self.assertEqual(self.characters.total_supply(), 0)
+
+    def test_character_creation_requires_character_creation_terminus_token_even_for_admins(
+        self,
+    ):
+        self.assertEqual(
+            self.terminus.balance_of(
+                self.admin, self.character_creation_terminus_pool_id
+            ),
+            0,
+        )
+
+        with self.assertRaises(VirtualMachineError):
+            self.characters.mint(self.admin.address, {"from": self.admin})
+
+        self.assertEqual(self.characters.total_supply(), 0)
+
+    def test_mint_to_self(self):
+        # Mint character creation badge to player account
+        self.terminus.mint(
+            self.player.address,
+            self.character_creation_terminus_pool_id,
+            10,
+            "",
+            self.owner_tx_config,
+        )
+
+        total_supply_0 = self.characters.total_supply()
+        character_creation_token_balance_0 = self.terminus.balance_of(
+            self.player.address, self.character_creation_terminus_pool_id
+        )
+        player_character_balance_0 = self.characters.balance_of(self.player.address)
+
+        self.characters.mint(self.player.address, {"from": self.player})
+
+        total_supply_1 = self.characters.total_supply()
+        character_creation_token_balance_1 = self.terminus.balance_of(
+            self.player.address, self.character_creation_terminus_pool_id
+        )
+        player_character_balance_1 = self.characters.balance_of(self.player.address)
+
+        self.assertEqual(total_supply_1, total_supply_0 + 1)
+        self.assertEqual(
+            character_creation_token_balance_1, character_creation_token_balance_0 - 1
+        )
+        self.assertEqual(player_character_balance_1, player_character_balance_0 + 1)
+
+    def test_mint_to_other(self):
+        # Mint character creation badge to player account
+        self.terminus.mint(
+            self.player.address,
+            self.character_creation_terminus_pool_id,
+            10,
+            "",
+            self.owner_tx_config,
+        )
+
+        total_supply_0 = self.characters.total_supply()
+        character_creation_token_balance_0 = self.terminus.balance_of(
+            self.player.address, self.character_creation_terminus_pool_id
+        )
+        player_character_balance_0 = self.characters.balance_of(self.player.address)
+        random_person_character_balance_0 = self.characters.balance_of(self.random_person.address)
+
+        self.characters.mint(self.random_person.address, {"from": self.player})
+
+        total_supply_1 = self.characters.total_supply()
+        character_creation_token_balance_1 = self.terminus.balance_of(
+            self.player.address, self.character_creation_terminus_pool_id
+        )
+        player_character_balance_1 = self.characters.balance_of(self.player.address)
+        random_person_character_balance_1 = self.characters.balance_of(self.random_person.address)
+
+        self.assertEqual(total_supply_1, total_supply_0 + 1)
+        self.assertEqual(
+            character_creation_token_balance_1, character_creation_token_balance_0 - 1
+        )
+        self.assertEqual(player_character_balance_1, player_character_balance_0)
+        self.assertEqual(random_person_character_balance_1, random_person_character_balance_0 + 1)
